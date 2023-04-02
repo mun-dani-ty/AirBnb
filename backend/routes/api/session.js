406bfbd9b0d4ec998e8625587d3361e6824a807c
Daniela Riefkohl <65625295+mun-dani-ty@users.noreply.github.com>
2023-04-01 19:58:30 -0500

    adding validations

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -3,6 +3,9 @@ const { setTokenCookie, restoreUser } = require('../../utils/auth');
 const { User } = require('../../db/models');
 const router = express.Router();
 
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation');
+
 // Log in
 router.post(
     '/',
