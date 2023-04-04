47950358689420f9ef69dfc8cad58f344510e139
Daniela Riefkohl <65625295+mun-dani-ty@users.noreply.github.com>
2023-04-03 19:40:27 -0500

    fixing app.js

--- backend/app.js
+++ backend/app.js
@@ -5,114 +5,72 @@ const cors = require('cors');
 const csurf = require('csurf');
 const helmet = require('helmet');
 const cookieParser = require('cookie-parser');
+const { environment } = require('./config')
+const isProduction = environment === 'production'
 const { ValidationError } = require('sequelize');
-//var bcrypt = require('bcryptjs');
-//Create a variable called isProduction that will be true if the environment is in production or not by checking the environment key in the configuration file (backend/config/index.js):
-const { environment } = require('./config');
-const isProduction = environment === 'production';
-
-//routes
-const routes = require('./routes');
-
-//Initialize the Express application:
-const app = express();
-
-//Connect the morgan middleware for logging information about requests and responses:
-app.use(morgan('dev'));
-
-//Add the cookie-parser middleware for parsing cookies and the express.json middleware for parsing JSON bodies of requests with Content-Type of "application/json"
+const app = express()
+app.use(morgan('dev'))
 app.use(cookieParser());
 app.use(express.json());
+const routes = require('./routes')
 
 
-// Security Middleware
+// security middlewares
 if (!isProduction) {
-    // enable cors only in development
     app.use(cors());
 }
-
-// helmet helps set a variety of headers to better secure your app
 app.use(
     helmet.crossOriginResourcePolicy({
         policy: "cross-origin"
     })
 );
 
-// Set the _csrf token and create req.csrfToken method
 app.use(
+
     csurf({
         cookie: {
             secure: isProduction,
             sameSite: isProduction && "Lax",
             httpOnly: true
         }
+
     })
+
 );
+app.use(routes)
 
-//Routes
-app.use(routes); // Connect all the routes
-
-
-// Catch unhandled requests and forward to error handler.
+// catch unhandeled requests
 app.use((_req, _res, next) => {
     const err = new Error("The requested resource couldn't be found.");
     err.title = "Resource Not Found";
-    err.errors = ["The requested resource couldn't be found."];
+    err.errors = { message: "The requested resource couldn't be found." };
     err.status = 404;
     next(err);
 });
-
-//If this resource-not-found middleware is called, an error will be created with the message "The requested resource couldn't be found." and a status code of 404. Afterwards, next will be invoked with the error. Remember, next invoked with nothing means that error handlers defined after this middleware will not be invoked. However, next invoked with an error means that error handlers defined after this middleware will be invoked.
-
-// Process sequelize errors
-//catches sequelize  database validation errors
-app.use((err, req, res, next) => {
-    (console.log("IN APP.JS PROCESS SEQUELIZE ERRORS MIDDLEARE"))
+// catch validation errors
+app.use((err, _req, _res, next) => {
     // check if error is a Sequelize error:
     if (err instanceof ValidationError) {
-
-        let errObj = {};
-        err.errors.forEach(e => {
-            let newKey = e.path;
-            let newVal = e.message;
-            errObj[newKey] = newVal;
-        })
-
-        err.message = 'Validation Error';
-        err.errors = errObj;
-        err.status = 400;
-
-        return res.json({
-            // title: err.title || 'Server Error',
-            message: err.message,
-            errors: err.errors,
-            statusCode: err.status
-            // stack: isProduction ? null : err.stack
-        })
-
-        // for (let error of err.errors) {
-        //   errors[error.path] = error.message;
-        // }
-        // err.title = 'Validation error';
-        // err.errors = errors;
+        let errors = {};
+        for (let error of err.errors) {
+            errors[error.path] = error.message;
+        }
+        err.title = 'Validation error';
+        err.errors = errors;
     }
     next(err);
 });
-
-//If the error that caused this error-handler to be called is an instance of ValidationError from the sequelize package, then the error was created from a Sequelize database validation error and the additional keys of title string and errors array will be added to the error and passed into the next error handling middleware.
-
-// Error formatter
+// error formatter. This decides what the client sees.
 app.use((err, _req, res, _next) => {
     res.status(err.status || 500);
-    console.error(err);
+    // console.error(err);
     res.json({
-        //title: err.title || 'Server Error',
+        title: err.title || 'Server Error',
         message: err.message,
         errors: err.errors,
-        statusCode: err.status,
-        //stack: isProduction ? null : err.stack
+        stack: isProduction ? null : err.stack
     });
 });
 
-//Export the app
+
 module.exports = app;
