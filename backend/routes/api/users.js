9ef28be4892b72bde69c369bae982d3794cd3ead
Daniela Riefkohl <65625295+mun-dani-ty@users.noreply.github.com>
2023-04-03 19:55:03 -0500

    fixed user api

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -1,47 +1,79 @@
-const express = require('express');
-const router = express.Router();
-
+//this file will hold the resources for the route paths beginning with /api/users
+const express = require('express')
 const { setTokenCookie, requireAuth } = require('../../utils/auth');
 const { User } = require('../../db/models');
-
+//following two lines are from phase 5
 const { check } = require('express-validator');
 const { handleValidationErrors } = require('../../utils/validation');
+const router = express.Router();
 
-//middleware that checks keys and validates them
+
+//validateSignup middleware
 const validateSignup = [
     check('email')
         .exists({ checkFalsy: true })
         .isEmail()
-        .withMessage('Please provide a valid email.'),
+        .withMessage('Invalid email'),
     check('username')
         .exists({ checkFalsy: true })
         .isLength({ min: 4 })
-        .withMessage('Please provide a username with at least 4 characters.'),
-    check('username')
-        .not()
-        .isEmail()
-        .withMessage('Username cannot be an email.'),
+        .withMessage('Username is required (must be at least 4 characters)'),
     check('password')
         .exists({ checkFalsy: true })
         .isLength({ min: 6 })
         .withMessage('Password must be 6 characters or more.'),
+    check('firstName')
+        .exists({ checkFalsy: true })
+        .isLength({ min: 1 })
+        .isString()
+        .withMessage('First Name is required'),
+    check('lastName')
+        .exists({ checkFalsy: true })
+        .isLength({ min: 1 })
+        .isString()
+        .withMessage('Last Name is required'),
     handleValidationErrors
 ];
 
-// Sign up
-router.post(
-    '/',
-    validateSignup,
-    async (req, res) => {
-        const { username, email, password, firstName, lastName } = req.body;
-        const user = await User.signup({ email, username, password, firstName, lastName });
+router.post('/', validateSignup, async (req, res) => {
+    const { email, password, username, firstName, lastName } = req.body;
 
-        await setTokenCookie(res, user);
+    try {
+        //if all is successful, perform and return the following
+        const user = await User.signup({ email, username, password, firstName, lastName });
+        const token = await setTokenCookie(res, user);
 
         return res.json({
-            user: user
+            id: user.id,
+            firstName: user.firstName,
+            lastName: user.lastName,
+            email: user.email,
+            username: user.username,
+            token
         });
+    } catch (error) {
+        //iterate over 'errors' array and check if error was due to an existing username or email
+        //if error was due to existing username or email, send appropriate error message and status code
+        if (error.errors && error.errors.length > 0) {
+            const errorObj = {};
+            error.errors.forEach((ele) => {
+                if (ele.path === "username") {
+                    errorObj.username = `User with that username already exists`;
+                } else if (ele.path === "email") {
+                    errorObj.email = `User with that email already exists`;
+                }
+            })
+            return res.status(403).json({
+                message: "User already exists",
+                statusCode: 403,
+                errors: errorObj
+            })
+        }
+        return res.status(500).json({
+            message: "Internal server error",
+            statusCode: 500
+        })
     }
-);
+})
 
 module.exports = router;
